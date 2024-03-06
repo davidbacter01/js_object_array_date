2c5ca35d1dac5ff6589dd28675d3819d7e2591ae
davidbacter01 <davidbacter01@gmail.com>
2024-03-06 20:08:48 +0200

    Add last examples of array

--- script.js
+++ script.js
@@ -32,3 +32,15 @@ console.log('myProducts after shift(): ', myProducts);
 // indexOf - gasirea indexului unui element din array
 const indexOfPantaloni = myProducts.indexOf('Pantaloni');
 console.log(`Elementul Pantaloni se afla in lista la indexul ${indexOfPantaloni}`);
+
+
+// subset al unui array
+const subsetOfMyProducts = myProducts.slice(1, 3);
+console.log('subset from 1 to 3 is', subsetOfMyProducts);
+
+myProducts.splice(2, 1, 'Camasa');
+console.log('After splice(2, 1, "Camasa"): ', myProducts);
+
+// concatenare de array-uri
+const concatenatedArrays = myProducts.concat(subsetOfMyProducts);
+console.log('after concat(subsetOfMyProducts): ', concatenatedArrays);
