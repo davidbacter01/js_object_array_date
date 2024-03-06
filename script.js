90f9716a15ddb06a66bc16cf06155faa2d57840d
davidbacter01 <davidbacter01@gmail.com>
2024-03-06 19:52:59 +0200

    Add array examples (WIP)

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('Hello World!');
+const myProducts = ['Camasa', 'Pantaloni', 'Manusi'];
+
+console.log('myProducts: ', myProducts);
+
+console.log(`First item in myProducts is ${myProducts[0]}`);
+console.log(`Length of myProducts array is ${myProducts.length}`);
+
+// adaugare de elemente
+const newItem = 'Pantofi';
+myProducts.push(newItem);
+console.log('myProducts after push: ', myProducts);
+
+myProducts.push(25);
+console.log('myProducts after push: ', myProducts);
+
+// modificarea unui element
+myProducts[0] = 'Tricou';
+console.log('myProducts after changing first item: ', myProducts);
+
+//stergerea unui element
+myProducts.pop();
+console.log('myProducts after pop-ing last item: ', myProducts);
+
+//adaugarea unui element la inceputul array-ului
+myProducts.unshift('Camasa');
+console.log('myProducts after unshift(): ', myProducts);
+
+//stergerea unui element de la inceputul array-ului
+myProducts.shift();
+console.log('myProducts after shift(): ', myProducts);
+
+// indexOf - gasirea indexului unui element din array
+const indexOfPantaloni = myProducts.indexOf('Pantaloni');
+console.log(`Elementul Pantaloni se afla in lista la indexul ${indexOfPantaloni}`);
