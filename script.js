b64d094e2e2ce0560111904bf895f19534a1ddc4
davidbacter01 <davidbacter01@gmail.com>
2024-03-11 18:37:53 +0200

    Add objects example

--- script.js
+++ script.js
@@ -44,3 +44,52 @@ console.log('After splice(2, 1, "Camasa"): ', myProducts);
 // concatenare de array-uri
 const concatenatedArrays = myProducts.concat(subsetOfMyProducts);
 console.log('after concat(subsetOfMyProducts): ', concatenatedArrays);
+
+
+// Obiecte
+
+let person = {
+    name: "John",
+    surname: "Doe",
+    address: {
+        street: "Cetatii",
+        number: 21,
+        appartment: "b1",
+        block: "7b",
+        tronson: 1,
+        zipCode: "100203",
+        city: "Cluj-Napoca",
+        country: "Romania",
+        county: "Cluj"
+    },
+    age: 32
+};
+
+console.log("person: ", person);
+
+console.log(`nume persoana: ${person.name}`);
+
+console.log(`cod postal ${person.address.zipCode}`);
+
+// undefined property
+console.log(person.appartment);
+
+// brackets notation
+console.log(`person surname: ${person["surname"]}`);
+
+// modificare valori in obiect
+person.name = "Ion";
+console.log('name: ', person.name);
+
+//comparare obiecte
+const obj1 = {a: 10, b: true};
+const obj2 = {a: 10, b: true};
+const obj3 = obj1;
+
+console.log(`obj1 === obj2 => ${obj1 === obj2}`);
+console.log(`obj1 === obj3 => ${obj1 === obj3}`);
+
+
+// comparare dupa proprietati
+const areEqual = obj1.a === obj2.a && obj2.b === obj3.b;
+console.log('areEqual: ', areEqual);
